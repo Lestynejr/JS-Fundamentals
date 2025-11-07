2b9587c73fe5527a09af2746bbe0b050ba22b8ad
Celestine Nnamdi <celestine.nnamdi@outlook.com>
2025-11-07 17:21:15 +0100

    Added script that prints 3 lines

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,5 @@
 console.log('C is fun');
 let statement = "is cool"
-
-console.log("Python", statement);
-
 let age = "javaJavaScript is amazing";
-
-    console.log("javaJavaScript is amazing")
+console.log("Python", statement);
+console.log("javaJavaScript is amazing")
